20e3b65791b58ee08423cb0ab61de912a1541865
Chris Herrmann <cherrmann@lemon-digital.com>
2022-08-19 03:11:58 +0200

    calculate subtotal and total

---
+++ src/components/productsList/temp.ts
@@ -0,0 +1,20 @@
+{
+  "4509563079435": {
+      "name": "product 1",
+      "price": 29.99,
+      "quantity": 2,
+      "subtotal": 59.98
+  },
+  "5678327300382": {
+      "name": "product 2",
+      "price": 90.49,
+      "quantity": 1,
+      "subtotal": 90.49
+  },
+  "8612061865613": {
+      "name": "product 3",
+      "price": 58.99,
+      "quantity": 2,
+      "subtotal": 117.98
+  }
+}
