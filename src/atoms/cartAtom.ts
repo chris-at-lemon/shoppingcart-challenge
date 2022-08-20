7921c6c6c8a865c84412255500e1f58390051754
Chris Herrmann <cherrmann@lemon-digital.com>
2022-08-20 14:39:52 +0200

    persist atom to local storage

--- src/atoms/cartAtom.ts
+++ src/atoms/cartAtom.ts
@@ -1,6 +1,11 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+const { persistAtom } = recoilPersist({
+  key: "qogitaCart", // this key is using to store data in local storage
+});
 
 export const CartState = atom({
   key: "cart",
   default: {},
+  effects_UNSTABLE: [persistAtom],
 });
