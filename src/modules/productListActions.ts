93da5b768056c69e8c59a6a7bcd574f2b20f3e41
Chris Herrmann <cherrmann@lemon-digital.com>
2022-08-23 13:01:02 +0200

    api route fix

--- src/modules/productListActions.ts
+++ src/modules/productListActions.ts
@@ -2,11 +2,11 @@ import { httpGet } from "../modules/http";
 import { Product } from "../types";
 
 export const getProducts = async (pageNumber: number, productList: Product[]) => {
-  const results = await httpGet(`http://localhost:3000/api/products?page=${pageNumber}`);
+  const results = await httpGet(`./api/products?page=${pageNumber}`);
   return results.response;
 };
 
 export const getSingleProduct = async (gtin: string) => {
-  const results = await httpGet(`http://localhost:3000/api/products/${gtin}`);
+  const results = await httpGet(`./api/products/${gtin}`);
   return results.response;
 };
